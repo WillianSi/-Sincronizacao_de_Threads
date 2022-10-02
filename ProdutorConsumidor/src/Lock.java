03785d5e6db09e0562d20cbcacb60e8d6b5774a3
Willian Silvestre <70384084+WillianSi@users.noreply.github.com>
2022-10-01 21:28:43 -0300

    Delete ProdutorConsumidor directory

--- ProdutorConsumidor/src/Lock.java
+++
@@ -1,7 +0,0 @@
-import java.util.concurrent.Semaphore;
-
-public class Lock {
-	// Semaforo Mutex
-	public final static Semaphore semCon = new Semaphore(0); 
-	public final static Semaphore semProd = new Semaphore(1);
-}
