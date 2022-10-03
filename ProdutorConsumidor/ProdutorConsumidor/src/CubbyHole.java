9bfeff043aaf6b8f995fe8cf896df6d9cf655ea7
Willian Silvestre <70384084+WillianSi@users.noreply.github.com>
2022-10-03 20:17:08 -0300

    Resolucao1

---
+++ ProdutorConsumidor/ProdutorConsumidor/src/CubbyHole.java
@@ -0,0 +1,21 @@
+
+public class CubbyHole {
+	private int content  = -1;
+	
+	public void get(int consumer) {
+		try {
+			Lock.semCon.acquire();
+			System.err.printf("Cosumidor %d consumiu %d \n", consumer, content);
+			Lock.semProd.release();
+		} catch (InterruptedException e) {}
+	}
+	
+	public void set(int producer, int value) {
+		try {
+			Lock.semProd.acquire();
+			content = value;
+			System.out.printf("Pordutor %d produziu %d \n", producer, content);
+			Lock.semCon.release();
+		} catch (InterruptedException e) {}
+	}
+}
